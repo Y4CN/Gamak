9c63f676e5bfa98bcc3ea56f22de7cd8c38a9b96
Yasin <vyasin96@gmail.com>
2023-04-12 12:11:04 +0330

    add get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:game_hacks_chat/constant/generallColor.dart';
-import 'package:game_hacks_chat/screens/mainScreen.dart';
+import 'package:game_hacks_chat/locator.dart';
 import 'package:game_hacks_chat/screens/splashScreen.dart';
 
-void main() {
+void main() async {
+  await initGet_it();
   runApp(const MyApp());
 }
 
