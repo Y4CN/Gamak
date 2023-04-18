a347de76d5f94f75bd395ccaf52e9c441d8901db
Yasin <vyasin96@gmail.com>
2023-04-18 19:21:42 +0330

    create categoryGameDataSource & repo

--- lib/data/datasource/categoryDataSource.dart
+++ lib/data/datasource/categoryDataSource.dart
@@ -1,12 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:game_hacks_chat/data/model/categoryModel.dart';
+import 'package:game_hacks_chat/data/model/gameProductModel.dart';
 import 'package:game_hacks_chat/locator.dart';
 import 'package:game_hacks_chat/screens/homeScreen.dart';
 import 'package:game_hacks_chat/utilities/errorHandler.dart';
 
 abstract class ICategoryDatasource {
   Future<List<CategoryModel>> getAllCategory();
+  Future<List<GameProductModel>> getCategoryGame(String gameId);
 }
 
 class CategoryDataSource extends ICategoryDatasource {
@@ -17,6 +19,26 @@ class CategoryDataSource extends ICategoryDatasource {
       var response = await _dio.get('collections/category_games/records');
       return response.data['items']
           .map<CategoryModel>((e) => CategoryModel.fromJson(e))
+          .toList();
+    } on DioError catch (ex) {
+      throw ErrorHandler(ex.response?.statusCode, ex.response?.data['message']);
+    } catch (ex) {
+      throw ErrorHandler(0, 'unknown erorr');
+    }
+  }
+
+  @override
+  Future<List<GameProductModel>> getCategoryGame(String gameId) async {
+    try {
+      Map<String, dynamic> q = {
+        'filter': 'category_id="$gameId"',
+      };
+      var response = await _dio.get(
+        'collections/games_product/records',
+        queryParameters: q,
+      );
+      return response.data['items']
+          .map<GameProductModel>((e) => GameProductModel.fromJson(e))
           .toList();
     } on DioError catch (ex) {
       throw ErrorHandler(ex.response?.statusCode, ex.response?.data['message']);
--- lib/data/repository/categoryRepository.dart
+++ lib/data/repository/categoryRepository.dart
@@ -1,11 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:game_hacks_chat/data/datasource/categoryDataSource.dart';
 import 'package:game_hacks_chat/data/model/categoryModel.dart';
+import 'package:game_hacks_chat/data/model/gameProductModel.dart';
 import 'package:game_hacks_chat/locator.dart';
 import 'package:game_hacks_chat/utilities/errorHandler.dart';
 
 abstract class ICategoryRepository {
   Future<Either<String, List<CategoryModel>>> getAllCategory();
+  Future<Either<String, List<GameProductModel>>> getCategoryGame(String catID);
 }
 
 class CategoryRepository extends ICategoryRepository {
@@ -14,6 +16,16 @@ class CategoryRepository extends ICategoryRepository {
   Future<Either<String, List<CategoryModel>>> getAllCategory() async {
     try {
       var response = await _datasource.getAllCategory();
+      return Right(response);
+    } on ErrorHandler catch (e) {
+      throw Left(e.message ?? 'خطای ناشناخته');
+    }
+  }
+  
+  @override
+  Future<Either<String, List<GameProductModel>>> getCategoryGame(String catID) async{
+     try {
+      var response = await _datasource.getCategoryGame(catID);
       return Right(response);
     } on ErrorHandler catch (e) {
       throw Left(e.message ?? 'خطای ناشناخته');
