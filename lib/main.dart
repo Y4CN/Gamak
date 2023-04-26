b68a1fe9cd7fdb34478735e0cef667a9c69947fd
Yasin <vyasin96@gmail.com>
2023-04-26 12:29:45 +0330

    init push_pole_notification & change pakage name

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:game_hacks_chat/constant/generallColor.dart';
 import 'package:game_hacks_chat/locator.dart';
-import 'package:game_hacks_chat/screens/registerScreen.dart';
+import 'package:pushpole/pushpole.dart';
 import 'package:game_hacks_chat/screens/splashScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await PushPole.initialize();
   await initGet_it();
   runApp(const MyApp());
 }
