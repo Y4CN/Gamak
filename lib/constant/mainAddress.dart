cc3756c43292b240648358fd921adfa74fcad356
Yasin <vyasin96@gmail.com>
2023-05-03 17:33:47 +0330

    change Address to Server Address

--- lib/constant/mainAddress.dart
+++ lib/constant/mainAddress.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 class MainAddress{
   MainAddress._();
-  static String apiAddress =Platform.isAndroid? 'http://10.0.2.2:8090/api/' : 'http://127.0.0.1:8090/api/';
+  static String apiAddress ="http://185.213.195.139:80/api/";
 }
